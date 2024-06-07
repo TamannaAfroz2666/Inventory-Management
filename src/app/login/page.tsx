061e0efb620a5dcab234409673759c60820b4caf
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-06-07 12:03:03 +0600

    add business development site

---
+++ src/app/login/page.tsx
@@ -0,0 +1,115 @@
+"use client"
+
+import { useState } from "react";
+import Image from 'next/image'
+import { FaRegUser } from "react-icons/fa6";
+import { BsEyeSlashFill } from "react-icons/bs";
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('admin');
+    const [password, setPassword] = useState('admin');
+    const [checkbox, setCheckbox] = useState('');
+    const [err, setErr] = useState({});
+    const emailHandler = (e: any) => {
+        setEmail(e.target.value);
+    }
+    const passwordHandler = (e: any) => {
+        setPassword(e.target.value);
+    }
+    const checkboxHandler = (e: any) => {
+        setCheckbox(e.target.value);
+
+    }
+
+    const validationForm = () => {
+        let storeData = {};
+        if (!email) {
+            storeData.email = 'Enter your email';
+        }
+
+        if (!password) {
+            storeData.password = 'password required'
+        }
+        setErr(storeData);
+        return Object.keys(storeData).length === 0;
+
+    }
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        validationForm();
+
+    }
+
+
+    return (
+        <div className="min-h-screen  w-[25%] m-auto " >
+
+            <div className="bg-stone-50 mt-[12rem] shadow-xl ">
+                <div className="p-12 flex justify-center items-center ">
+                    <Image
+                        src="/Image/login-logo.png"
+                        width={100}
+                        height={100}
+                        alt="Picture of the author"
+
+                    />
+                </div>
+                <div className="">
+                    <div >
+                        <form className="max-w-sm mx-auto py-12" onSubmit={handleSubmit}>
+                            <div className="mb-5">
+                                <div className="flex">
+                                    <input
+                                        type="text"
+                                        id="email"
+                                        onChange={emailHandler}
+                                        name="email"
+                                        value={email}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                        focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                        dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder=""
+                                    />
+                                    <span className="right-8 relative mt-3 "> <FaRegUser size={16} color="gray" /></span>
+                                </div>
+                                {err.email && <p>{err.email}</p>}
+                            </div>
+                            <div className="mb-5">
+                                <div className="flex"> 
+                                    <input type="password" id="password" name="password" value={password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                        focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
+                         dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        onChange={passwordHandler} />
+                                    <span className="right-8 relative mt-3 "> <BsEyeSlashFill size={16} color="gray" /></span>
+                                </div>
+                                {err.password && <p>{err.password}</p>}
+                            </div>
+                            <div className="flex items-start mb-5">
+                                <div className="flex items-center h-5">
+
+                                    <a href="/forget-password">
+                                        <span className="text-blue-600/100 text-[14px]"> Forget Password ? </span>
+                                    </a>
+
+                                </div>
+
+                            </div>
+                            <button type="submit" className="text-white w-90 p-2.5 my-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+
+            </div>
+
+        </div>
+    );
+};
+
+export default LoginPage;
