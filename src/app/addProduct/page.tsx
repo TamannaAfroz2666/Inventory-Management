4ccb382f6a3d758492b2f3f8945189e38dadb8f1
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-10-22 16:54:32 +0600

    Fixed the layout issue

--- src/app/addProduct/page.tsx
+++ src/app/addProduct/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import DashboardLayout from '../dashboard/page';
 
 const page = () => {
-    return (
+    return (<>
+    <DashboardLayout> 
         <div>
-            <h1>hello out of component section in add product pagew</h1>
+            <h1 className='text-rose-500 mt-[4rem]'>hello out of component section in add product page</h1>
         </div>
+        </DashboardLayout>
+        </>
     );
 };
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,38 +1,21 @@
-
 'use client';
 import React from 'react';
 import { Layout } from 'antd';
-// import SideNavigationBar from './SideNavigationBar'; // Import your sidebar
-// import { useRouter } from 'next/router';
 import SideNavigationBar from '../components/SideNavigationBar/page';
 
-const { Header, Sider, Content } = Layout;
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-    // const router = useRouter();
-
+const DashboardLayout = ({ children }) => {
     return (
         <>
-        {/* <Layout style={{ minHeight: '100vh' }} >
-            <Sider width={256}>
-             
-            </Sider>
-            <Layout>
-                <Header style={{ background: '#fff', padding: 0 }}>
-                    <div className="text-center text-xl font-bold">Dashboard Header</div>
-                </Header>
-                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-                    <div style={{ padding: 24, background: '#fff', minHeight: '360px' }}>
-                        {children}
-                    </div>
-                </Content>
-            </Layout>
-        </Layout> */}
-        <div>
-        <SideNavigationBar />
-        </div>
+            <div className="dashboard-container flex gap-2 w-full">
+                <div className="sidebar w-[12%]">
+                    <SideNavigationBar />
+                </div>
+                <div className="main-content">
+                    {children}
+                </div>
+            </div>
         </>
-        
     );
 };
 
